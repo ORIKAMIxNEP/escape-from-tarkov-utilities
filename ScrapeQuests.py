97f8622eb14834965242841499b6b55d8cd4246f
ORIKAMIxNEP <taiki.orikami@gmail.com>
2023-10-18 10:59:46 +0900

    format

--- ScrapeQuests.py
+++ ScrapeQuests.py
@@ -6,13 +6,13 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 options = webdriver.ChromeOptions()
 options.add_argument("--headless")
-driver = webdriver.Chrome(service=Service(
-    ChromeDriverManager().install()), options=options)
+driver = webdriver.Chrome(
+    service=Service(ChromeDriverManager().install()), options=options
+)
 
 tradersList = []
 driver.get("https://wikiwiki.jp/eft/")
-traders = driver.find_elements(
-    By.XPATH, value="//*[@id='menubar']/ul[2]/li[7]/ul/li/a")
+traders = driver.find_elements(By.XPATH, value="//*[@id='menubar']/ul[2]/li[7]/ul/li/a")
 for trader in traders:
     if trader.text != "Fence":
         tradersList.append(trader.text)
@@ -20,14 +20,14 @@ for trader in traders:
 questURLs = {traderName: [] for traderName in tradersList}
 for traderName in tradersList:
     driver.get("https://wikiwiki.jp/eft/" + traderName)
-    quests = driver.find_elements(
-        By.XPATH, value="//*[@id='content']/ul[1]/li/a")
+    quests = driver.find_elements(By.XPATH, value="//*[@id='content']/ul[1]/li/a")
     for quest in quests:
         questURLs[traderName].append(quest.get_attribute("href"))
 
 spreadsheetsData = ""
-questCount = {traderName: {"allCount": 0, "validCount": 0}
-              for traderName in tradersList}
+questCount = {
+    traderName: {"allCount": 0, "validCount": 0} for traderName in tradersList
+}
 errorQuestPages = []
 for traderName in tradersList:
     spreadsheetsData += traderName + "\n"
@@ -35,12 +35,13 @@ for traderName in tradersList:
         questCount[traderName]["allCount"] += 1
         driver.get(questURL)
         try:
-            questName = driver.find_element(
-                By.XPATH, value="//*[@id='title']/h1").text
+            questName = driver.find_element(By.XPATH, value="//*[@id='title']/h1").text
             if "/" in questName:
-                questName = questName[questName.index("/") + 1:]
+                questName = questName[questName.index("/") + 1 :]
             kappaRequired = driver.find_element(
-                By.XPATH, value="//*[@id='content']//td[contains(text(), 'Kappaタスク')]/following-sibling::td[1]").text
+                By.XPATH,
+                value="//*[@id='content']//td[contains(text(), 'Kappaタスク')]/following-sibling::td[1]",
+            ).text
         except:
             errorQuestPages.append(questURL)
             continue
@@ -48,10 +49,10 @@ for traderName in tradersList:
         spreadsheetsData += questName + "\t" + kappaRequired + "\n"
 
 for traderName in tradersList:
-    print(traderName + "'s AllQuestPage: " +
-          str(questCount[traderName]["allCount"]))
-    print(traderName + "'s ValidQuestPage: " +
-          str(questCount[traderName]["validCount"]))
+    print(traderName + "'s AllQuestPage: " + str(questCount[traderName]["allCount"]))
+    print(
+        traderName + "'s ValidQuestPage: " + str(questCount[traderName]["validCount"])
+    )
 
 print("エラータスクページ：", end="")
 for i, errorQuestPage in enumerate(errorQuestPages):
